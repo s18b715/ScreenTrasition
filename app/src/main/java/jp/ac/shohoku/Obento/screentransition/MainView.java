c3cf99ad8faa5a78be817a60b5f5ef27ff62ccf5
s18b717 <s18b717@shohoku.ac.jp>
2019-10-10 15:14:24 +0900

    MainViewの作成

---
+++ app/src/main/java/jp/ac/shohoku/Obento/screentransition/MainView.java
@@ -0,0 +1,19 @@
+package jp.ac.shohoku.Obento.screentransition;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+class MainView extends View {
+    public MainView(Context context) {
+        this(context, null);
+    }
+
+    public MainView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MainView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
