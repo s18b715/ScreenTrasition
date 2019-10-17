47ea69e1f8ac908585e138da0363c5d8f6ea4987
s18b715 <s18b715@shohoku.ac.jp>
2019-10-17 14:23:09 +0900

    MainActivityの変更 activity_view.xmlの作成

--- app/src/androidTest/java/jp/ac/shohoku/Obento/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/jp/ac/shohoku/Obento/ExampleInstrumentedTest.java
@@ -1,7 +1,6 @@
 package jp.ac.shohoku.Obento;
 
 import android.content.Context;
-
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
--- app/src/main/java/jp/ac/shohoku/Obento/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/Obento/MainActivity.java
@@ -1,14 +1,22 @@
 package jp.ac.shohoku.Obento;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+
+    int count = 1;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_view);
+        //ボタンを押したときにイベントを取得できるようにする
+
     }
 }
