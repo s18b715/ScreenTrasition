a5e33189f0ecbd784105d76fd1e65d30c7f03cbe
s18b717 <s18b717@shohoku.ac.jp>
2019-10-17 15:25:03 +0900

    MainActivityの追記とview1とview2の修正

--- app/src/main/java/jp/ac/shohoku/Obento/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/Obento/MainActivity.java
@@ -2,6 +2,9 @@ package jp.ac.shohoku.Obento;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +20,25 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
         //ボタンを押したときにイベントを取得できるようにする
-
+        Button button1 = (Button) findViewById(R. id. button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (count == 1) {
+                    //変更したいレイアウトを取得する
+                    LinearLayout layout = (LinearLayout) findViewById(R.id.linearlayout1);
+                    //レイアウトのビューをすべて削除する
+                    layout.removeAllViews();
+                    //レイアウトをR. layout. sampleに変更する
+                    getLayoutInflater().inflate(R.layout.view2, layout);
+                    count = 2;
+                } else if (count == 2) {
+                    LinearLayout layout = (LinearLayout) findViewById(R.id.linearlayout2);
+                    layout.removeAllViews();
+                    getLayoutInflater().inflate(R.layout.view1, layout);
+                    count = 1;
+                }
+            }
+        });
     }
 }
